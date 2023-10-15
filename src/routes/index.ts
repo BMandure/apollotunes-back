068e95a18eabe7e6b56039c8674a5af05601644c
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-10-15 16:08:44 -0300

    Add some GET routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,24 @@ import { getSongs } from "../services/routes";
 
 const routes = express.Router();
 
-routes.get("/", (_, res) => {
+routes.get("/all-songs", (_, res) => {
   res.send(getSongs());
 });
 
+routes.get("/all-albums", (_, res) => {
+  res.send("Get all albums");
+});
+
+routes.get("/all-artists", (_, res) => {
+  res.send("Get all artists");
+});
+
+routes.get("/all-users", (_, res) => {
+  res.send("Get all users");
+});
+
 routes.post("/", (_, res) => {
-  res.send("Prueba post /");
+  res.send("POST Test at '/'");
 });
 
 export default routes;
